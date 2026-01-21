9771abdf5c4fb7e77ede05a578c25070e074074f
weberfern <weber12@gmail.com>
2026-01-20 21:43:23 -0300

    Restore /alunos/ in console.log message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${port}/alunos`);
 });
 
 let alunos = [
